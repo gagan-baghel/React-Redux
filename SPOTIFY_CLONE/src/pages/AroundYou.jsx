2f1dd2ebaa1a14719bcfd8972ccd317cc6f0082a
MACBOOK PRO <gagansinghbaghel15@gmail.com>
2026-08-27 17:50:17 +0530

    Move geo API key to env var, remove hardcoded secret

--- SPOTIFY_CLONE/src/pages/AroundYou.jsx
+++ SPOTIFY_CLONE/src/pages/AroundYou.jsx
@@ -10,7 +10,7 @@ const AroundYou = () => {
   const [loading, setLoading] = useState(true);
   const { activeSong, isPlaying } = useSelector((state) => state.player);
   useEffect(() => {
-    axios.get('https://geo.ipify.org/api/v2/country?apiKey=REDACTED_API_KEY').then((res) => setCountry(res?.data?.location?.country)).catch((err) => console.log(err)).finally(() => setLoading(false));
+    axios.get(`https://geo.ipify.org/api/v2/country?apiKey=${import.meta.env.VITE_GEO_API_KEY}`).then((res) => setCountry(res?.data?.location?.country)).catch((err) => console.log(err)).finally(() => setLoading(false));
   }, [country]);
 
   const { data, isFetching, error } = useGetSongsByCountryQuery(country);
